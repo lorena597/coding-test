8e9a12435661403d011d98c8a5c7e74229944dd4
lorena597 <lorenalee_597@163.com>
2022-11-21 12:16:10 +0800

    second version

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import spearmanr
 
 class Strategy(object):
 
@@ -13,45 +14,77 @@ class Strategy(object):
     
     def signal1(self, window):
         return self.price / self.price.shift(window) - 1.
-
-    def signal2(self, window):
-        daily_return_abs = abs(self.daily_return)
-        path = daily_return_abs.rolling(window, min_periods = int(window/4)).sum()
-        distance = self.price / self.price.shift(window) - 1.
-        distance[distance == 0] = np.nan
-        return path / distance
+    
+    def signal2(self, long, short):
+        return self.price.shift(short) / self.price.shift(long) - 1.
     
     def signal3(self, window):
         H = self.price.rolling(window, min_periods = int(window/4)).max()
         L = self.price.rolling(window, min_periods = int(window/4)).min()
-        twap = self.price.rolling(window, min_periods = int(window/4)).mean()
-        return (twap - L) / (H - L)
+        # H = self.price.rolling(window).max()
+        # L = self.price.rolling(window).min()
+        pos = (self.price - L) / (H - L)
+        pos[H - L <= 0.01] = np.nan
+        return pos
     
     def signal4(self, window):
-        return 1. / self.daily_return.rolling(window, min_periods = int(window/4)).std()
-    
-    def signal5(self, long, short):
-        return self.price.shift(short) / self.price.shift(long) - 1.
-
-    def signal6(self, window):
-        return 1. / self.daily_return.rolling(window, min_periods = int(window/4)).skew()
-
-    def signal7(self, window):
-        return 1. / self.daily_return.rolling(window, min_periods = int(window/4)).kurt()
-    
-    def signal8(self, window):
         H = self.price.rolling(window, min_periods = int(window/4)).max()
         L = self.price.rolling(window, min_periods = int(window/4)).min()
-        return (self.price - L) / (H - L)
+        twap = self.price.rolling(window, min_periods = int(window/4)).mean()
+        pos = (twap - L) / (H - L)
+        pos[H - L <= 0.01] = np.nan
+        return pos
+    
+    def signal5(self, window):
+        return self.daily_return.rolling(window, min_periods = int(window/4)).std()
+
+    def signal6(self, window):
+        return self.daily_return.rolling(window, min_periods = int(window/4)).skew()
+
+    def signal7(self, window):
+        return self.daily_return.rolling(window, min_periods = int(window/4)).kurt()
+
+    def signal8(self, window):
+        daily_return_abs = abs(self.daily_return)
+        path = daily_return_abs.rolling(window).sum()
+        distance = self.price / self.price.shift(window) - 1.
+        distance[distance <= 0.01] = np.nan
+        return path / distance
 
     def signal9(self, window):
-        # to do: corr between price and volume
-        pass
+        def corr_by_column(x, y):
+            mu_x = np.nanmean(x, axis = 0)
+            mu_y = np.nanmean(y, axis = 0)
+            sigma_x = np.nanstd(x, axis = 0)
+            sigma_y = np.nanstd(y, axis = 0)
+            return np.nanmean((x - mu_x) * (y - mu_y), axis = 0) / (sigma_x * sigma_y)
+        corr = np.zeros(shape = self.price.shape); corr[:] = np.nan
+        for i in range(corr.shape[0]):
+            cur_price = self.price.iloc[(i-window+1):(i+1),:]
+            cur_volume = self.volume.iloc[(i-window+1):(i+1),:]
+            corr[i,:] = corr_by_column(cur_price, cur_volume)
+        corr_df = pd.DataFrame(corr, index = self.price.index, columns = self.price.columns)
+        return corr_df
     
     def signal10(self, window):
-        # to do: about smart money
-        pass
+        def calc(p, v, s, w):
+            vwap = np.nansum(p * v, axis = 0) / np.nansum(v, axis = 0)
+            ix = np.argsort(s.values, axis = 0)
+            v_sort = np.take_along_axis(v.values, ix, axis = 0)
+            p_sort = np.take_along_axis(p.values, ix, axis = 0)
+            vwap_1 = np.nansum(v_sort[-int(w/4):,:] * p_sort[-int(w/4):,:], axis = 0) / np.nansum(v_sort[-int(w/4):,:], axis = 0)
+            return vwap_1/vwap
 
+        S = np.abs(self.daily_return) / np.sqrt(self.volume)
+        S = pd.DataFrame(S, index = self.price.index, columns = self.price.columns)
+        factor = np.zeros(shape = self.price.shape); factor[:] = np.nan
+        for i in range(factor.shape[0]):
+            cur_price = self.price.iloc[(i-window+1):(i+1),:]
+            cur_volume = self.volume.iloc[(i-window+1):(i+1),:]
+            cur_smart = S.iloc[(i-window+1):(i+1),:]
+            factor[i,:] = calc(cur_price,cur_volume,cur_smart,window)
+        factor_df = pd.DataFrame(factor, index = self.price.index, columns = self.price.columns)
+        return factor_df
 
 class Backtest(object):
 
@@ -60,27 +93,56 @@ class Backtest(object):
         self.all_dates = self.price.index
         self.all_tickers = self.price.columns
         self.tradable_matrix = ~np.isnan(self.price)
+        self.all_dates_int = np.array([int(''.join(date.split('-'))) for date in self.all_dates])
+        self.all_years_int = np.array([date_int // 10000 for date_int in self.all_dates_int])
 
     def get_result(self, signal: pd.DataFrame):
+        signal = signal.copy()
         daily_return = self.price / self.price.shift(1) - 1.
         daily_return[~self.tradable_matrix] = np.nan
         signal[~self.tradable_matrix] = np.nan
-        transform = self.normalize(self.winsorize(signal))
+        # transform = self.normalize(self.winsorize(signal))
+        transform = self.scale(self.winsorize(signal))
         transform = transform / np.nansum(transform * (transform > 0), axis = 1).reshape((-1,1))
         mask = np.nansum(~np.isnan(transform), axis = 1) == 0
         tot_rtn = np.nansum(pd.DataFrame(transform).shift(1).values * daily_return, axis = 1)
         tot_rtn[mask] = np.nan
         ix = np.array([i for i in range(len(tot_rtn)) if i % 10 == 0])
-        stats_dict = self.calculate_stats(tot_rtn[ix], self.all_dates[ix])
+        stats_dict = self.calculate_stats(tot_rtn[ix], self.all_dates_int[ix])
         # stats_dict = self.calculate_stats(tot_rtn, self.all_dates)
-        return stats_dict
+        # calculate stats for every year
+        dict_list = []
+        unique_years = np.unique(self.all_years_int)
+        for unique_year in unique_years:
+            mask = self.all_years_int == unique_year
+            cur_rtn = tot_rtn[mask]
+            cur_dates = self.all_dates_int[mask]
+            ix = np.array([i for i in range(len(cur_rtn)) if i % 10 == 0])
+            dict_list.append(self.calculate_stats(cur_rtn[ix], cur_dates[ix]))
+        dict_list.append(stats_dict)
+        result = pd.DataFrame(dict_list, index = list(unique_years) + ['total'])
+        # ic = self.calculate_ic(signal, daily_return)
+        # stats_dict['IC'] = ic
+        return result.round(2)
+    
+    @staticmethod
+    def calculate_ic(signal, daily_return):
+        corr_ts = []
+        for i in range(len(signal)-1):
+            cur_signal = signal.iloc[i,:]
+            cur_return = daily_return.iloc[i+1,:]
+            mask = ~np.isnan(cur_signal) & ~np.isnan(cur_return)
+            cur_signal = cur_signal[mask]
+            cur_return = cur_return[mask]
+            corr_ts.append(spearmanr(cur_signal, cur_return))
+        return np.mean(corr_ts)
     
     @staticmethod
     def calculate_stats(rtn,ds):
         ix = np.where(~np.isnan(rtn))[0][0]
         prd_rtn = np.nansum(rtn[ix:])
-        ann_rtn = np.nanmean(rtn[ix:]) * 25
-        ann_vol = np.nanstd(rtn[ix:]) * np.sqrt(25) 
+        ann_rtn = np.nanmean(rtn[ix:]) * 252
+        ann_vol = np.nanstd(rtn[ix:]) * np.sqrt(252) 
         shrp = ann_rtn / ann_vol
         nav = np.cumsum(rtn[ix:])
         mdd, mdd_bgn, mdd_end = 0, 0, 0
@@ -104,6 +166,13 @@ class Backtest(object):
     @staticmethod
     def normalize(signal):
         return (signal - np.nanmean(signal, axis = 1).reshape(-1, 1)) / np.nanstd(signal, axis = 1).reshape(-1, 1)
+    
+    @staticmethod
+    def scale(signal):
+        signal = signal.rank(axis = 1)
+        min_ts = np.nanmin(signal, axis = 1).reshape(-1,1)
+        max_ts = np.nanmax(signal, axis = 1).reshape(-1,1)
+        return (signal - min_ts) / (max_ts - min_ts) - 0.5
     
     @staticmethod
     def winsorize(signal):
